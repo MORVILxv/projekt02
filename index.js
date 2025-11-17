df38bad7cbfb7ae0d8783849c1c82abbaa41ff46
MORVILxv <morvilxv@gmail.com>
2025-11-17 22:29:43 +0100

    final

--- index.js
+++ index.js
@@ -1,57 +1,29 @@
-// import express from "express";
+import express from "express";
 
-// const port = 8000;
-// const categories = ["Stolice europejskie", "English - food", "Kasane Teto", "War Thunder"];
-
-// const app = express(); // Tworzymy obiekt aplikacji/serwera Express
-
-// app.set("view engine", "ejs");
-
-// app.use(express.static("public"));
-
-// app.get("/cards/categories/", (req, res) => {
-//     res.render("categories", { title: "Kategorie", categories: categories });
-// })
-
-// app.get("/", (req, res) => {
-//     res.render("skibidi");
-// })
-
-// app.listen(port, () => { // Startujemy serwer na porcie 8000
-//     console.log(`Server listening on http://localhost:${port}`);
-// });
-
-const tanks = {
-    "M4A3 76 W": {
-        name: "M4A3 76 W",
-        nation: "USA",
-        gun_caliber: "76mm",
-        crew: "5",
-        horsepower: "500",
-        ammo_types: ["AP", "APCBC", "HVAP", "HE"]
-    },
-    "T-34-85": {
-        name: "T-34-85",
-        nation: "USSR",
-        gun_caliber: "85mm",
-        crew: "4",
-        horsepower: "500",
-        ammo_types: ["APHE", "APHEBC", "HVAP", "HE"]
-    }
+const tankMuseum = {
+    name: "The Great Tank Museum of Szczecin",
+    tanks: [
+        ["USA", "M4A3"],
+        ["USSR", "T-34-85"], 
+        ["Germany", "Tiger H1"]
+    ]
+};
+function getTanks() {
+    return [tankMuseum.name, tankMuseum.tanks];
 }
 
-function getTanks() {
-    Object.entries(tanks).map(([id, tank]) => {
-        return {id, tank: tank.name};
-    });
-};
+function addTank(country, tankName) {
+    tankMuseum.tanks.push([country, tankName])
+}
 
 
-
-
-
-import express from "express";
-import flashcards from "./models/flashcards.js";
+const about = {
+    name: "About the Museum",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam fermentum suscipit magna, nec laoreet velit eleifend sed. Suspendisse et enim nibh. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum auctor varius turpis vitae consectetur. Praesent volutpat gravida nibh sit amet sodales. Ut porttitor est quis efficitur rutrum. Vestibulum ac quam euismod, elementum libero vitae, rutrum nunc. Nullam pharetra leo consequat felis tempus, sed molestie nunc iaculis. Duis efficitur imperdiet pharetra. Vestibulum in lacinia orci. Nulla et mauris sit amet elit vehicula pharetra nec quis urna. Praesent quam elit, tempor iaculis vestibulum at, sagittis vitae sapien. In aliquet augue ac porttitor dictum"
+}
+function getAbout() {
+    return [about.name, about.text];
+}
 
 const port = 8000;
 
@@ -60,36 +32,42 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded());
 
-app.get("/cards", (req, res) => {
-    res.render("categories", {
-        title: "Kategorie",
-        categories: flashcards.getCategorySummaries(),
-    });
+app.get("/all", (req, res) => {
+
+    res.render("names", {
+        name: "Subsites", 
+        mname: getTanks()[0],
+        aname: getAbout()[0]
+    })
 });
 
-app.get("/cards/:category_id", (req, res) => {
-    const category = flashcards.getCategory(req.params.category_id);
-    if (category != null) {
-        res.render("category", {
-            title: category.name,
-            category,
+app.get("/all/tankmuseum", (req, res) => {
+    const data = getTanks();
+    if (data != null) {
+        res.render("tanks", {
+            name: "List of tanks", 
+            data: data,
         });
     } else {
         res.sendStatus(404);
     }
 });
 
-app.post("/cards/:category_id/new", (req, res) => {
-    const category_id = req.params.category_id;
-    if (!flashcards.hasCategory(category_id)) {
-        res.sendStatus(404);
-    } else {
-        flashcards.addCard(category_id, {
-            front: req.body.front,
-            back: req.body.back,
+app.get("/all/about", (req, res) => {
+    const data = getAbout();
+    if (data != null) {
+        res.render("abt", {
+            name: "About Us", 
+            data: data,
         });
-        res.redirect(`/cards/${category_id}`);
+    } else {
+        res.sendStatus(404);
     }
+});
+
+app.post("/all/tankmuseum/new", (req, res) => {
+    addTank(req.body.country, req.body.tankName);
+    res.redirect(`/all/tankmuseum`);
 });
 
 app.listen(port, () => {
